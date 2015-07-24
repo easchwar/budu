3b5dac1d5d8f4ad89d033c8af9808ddc49f9c952
easchwar <easchwarzenbach@gmail.com>
2015-07-24 13:48:11 -0700

    add arrays

--- lib/budu.rb
+++ lib/budu.rb
@@ -1,5 +1,11 @@
 require "budu/version"
+require "budu/array"
 
 module Budu
-  # Your code goes here...
+  class Application
+    def call(env)
+      `echo debug > debug.txt`;
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+    end
+  end
 end
