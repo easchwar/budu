70c7f393d412bd996a6f0b8c713ef50203c3966a
easchwar <easchwarzenbach@gmail.com>
2015-08-09 09:13:48 -0700

    import orm files, require sqlite3

---
+++ lib/budu/orm/db_connection.rb
@@ -0,0 +1,55 @@
+require 'sqlite3'
+
+# https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
+ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
+MODEL_SQL_FILE = File.join(ROOT_FOLDER, 'local_db.sql')
+MODEL_DB_FILE = File.join(ROOT_FOLDER, 'local_db.db')
+
+module Budu
+  class DBConnection
+    def self.open(db_file_name)
+      @db = SQLite3::Database.new(db_file_name)
+      @db.results_as_hash = true
+      @db.type_translation = true
+
+      @db
+    end
+
+    def self.reset
+      commands = [
+        "rm '#{MODEL_DB_FILE}'",
+        "cat '#{MODEL_SQL_FILE}' | sqlite3 '#{MODEL_DB_FILE}'"
+      ]
+
+      commands.each { |command| `#{command}` }
+      DBConnection.open(MODEL_DB_FILE)
+    end
+
+    def self.instance
+      reset if @db.nil?
+
+      @db
+    end
+
+    def self.execute(*args)
+      puts args[0]
+
+      instance.execute(*args)
+    end
+
+    def self.execute2(*args)
+      puts args[0]
+
+      instance.execute2(*args)
+    end
+
+    def self.last_insert_row_id
+      instance.last_insert_row_id
+    end
+
+    private
+
+    def initialize(db_file_name)
+    end
+  end
+end
